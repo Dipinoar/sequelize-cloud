81b6ee6e85e0571ff5bd5ae6ae027820bd997737
Dipinoar <113305509+Dipinoar@users.noreply.github.com>
2023-03-04 21:42:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs');
-const dotenv = require('dotenv')
+require("dotenv").config({path:"./.env"})
 //importamos Helpers
 var helpers = require('./components/hbsHelpers');
 
